b2d8f6b780daaa21f7776b46960baceb6e0952e7
Brumadu <madureira12.bm@gmail.com>
2024-10-04 15:42:42 -0300

    fix: header not aligned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,12 +48,14 @@ export default async function Home() {
 
   return (
     <>
-      <header></header>
+      <header className="flex h-10% items-center justify-start"></header>
       <div className="flex gap-10 h-85% md-max:flex-wrap md-max:h-[300%] xxl:text-4xl sm-max:text-xs">
         <div className="flex basis-3/5 md-max:basis-full rounded-lg shadow-lg">
           <div className="rounded-lg w-100% bg-gradient-to-b from-green-800 to-green-900 ">
             <div className=" w-100% h-10% flex items-center justify-center">
-              <div className=" xxl:text-7xl lg:text-3xl">Session List</div>
+              <div className=" xxl:text-7xl lg:text-3xl font-bold ">
+                SESSION LIST
+              </div>
             </div>
             <div className="self-end w-100% h-90% p-2 rounded-b-3xl rounded-t-md">
               <div className="flex border  border-black rounded-md bg-slate-500 justify-around text-center text-black mb-2">
@@ -68,11 +70,13 @@ export default async function Home() {
           </div>
         </div>
         <div className="flex basis-2/5 md-max:basis-full rounded-lg text-center shadow-lg ">
-          <div className="rounded-lg w-100% bg-gradient-to-b from-green-800 to-green-900">
-            <div className=" text-center my-3 stroke-3 stroke-black text-white xxl:text-7xl lg:text-3xl ">
-              Create New Session
+          <div className="flex flex-col h-100% items-center  rounded-lg w-100% bg-gradient-to-b from-green-800 to-green-900">
+            <div className="flex items-center text-center h-10% stroke-3 stroke-black text-white xxl:text-7xl lg:text-3xl font-bold ">
+              CREATE NEW SESSION
             </div>
-            <CreateServerArea />
+            <div className="h-90% w-[60%]">
+              <CreateServerArea />
+            </div>
           </div>
         </div>
       </div>
